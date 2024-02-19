3bf29caff6155f94735760517fa9d00fa4a004e5
Chalaga <150872963+Chalaga@users.noreply.github.com>
2024-02-20 01:49:57 +0400

    Added another Classwork

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-// 2)შექმენით Animal კლასი კონსტრუქტორით, რომელიც იღებს სახელს და ხმას პარამეტრებად. დაამატეთ makeSound მეთოდი, რომელიც ბეჭდავს ცხოველის მიერ გამოშვებულ ხმას.
+// // // 2)შექმენით Animal კლასი კონსტრუქტორით, რომელიც იღებს სახელს და ხმას პარამეტრებად. დაამატეთ makeSound მეთოდი, რომელიც ბეჭდავს ცხოველის მიერ გამოშვებულ ხმას.
 
-class Animal {
-    constructor(Name, Sound) {
-        this.Name = Name
-        this.Sound = Sound
+// // class Animal {
+// //     constructor(Name, Sound) {
+// //         this.Name = Name
+// //         this.Sound = Sound
 
-    }
+// //     }
 
-    makesound() {
-        console.log(this.Sound);
-    }
-}
+// //     makesound() {
+// //         console.log(this.Sound);
+// //     }
+// // }
+
+// // 1)დავალება: შექმენით კლასი სახელად Book კონსტრუქტორით, რომელიც იღებს ორ არგუმენტს: სათაური და ავტორი. დაამატეთ მეთოდი სახელად getDetails, რომელიც აბრუნებს სტრიქონს, რომელიც აერთიანებს წიგნის სათაურსა და ავტორს.
+
+// class Book {
+//     constructor (title , author) {
+//         this.title  = title 
+//         this.author = author
+//     }
+
+//     getDetails() {
+//         return `This ${this.title} is written by ${this.author}`
+//     } 
+// }
+
+// const ThisBook = new Book();
+// ThisBook.title = '"The Right Hand of the Grand Master"'
+// ThisBook.author = ' Konstantine Gamsaxurdia'
+// console.log(ThisBook.getDetails());
